7c98b0229cd4b3091cb61605973f90293d86bc9b
nerdwithcomputers <hpscigeek@gmail.com>
2024-08-12 10:45:04 -0400

    i've horribly broken something

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import fs from "fs";
 
+var stack = [];
+var str = [];
+var strStat;
+let funcs = new Map();
+var inProgFunc = [];
+// var funcIndex;
+var funcStat = false;
+
 class splitspace{
     [Symbol.split](string){
         let s1 = string.split(" ");
@@ -28,6 +36,87 @@ new Promise((resolve, reject) => {
     });
 });
 
+function exec(x){
+    switch(x){
+        case "": break;
+        case "scribe":{
+            funcStat = true;
+            break;
+        }
+        case "cut":{
+            funcStat = false;
+            console.log
+            break;
+        }
+        case "+":{
+            stack[stack.length-2] = parseInt(stack[stack.length-1]) + parseInt(stack[stack.length-2]);
+            stack.pop();
+            break;
+        }
+        case "-":{
+            stack[stack.length-2] = parseInt(stack[stack.length-1]) - parseInt(stack[stack.length-2]);
+            stack.pop();
+            break;
+        }
+        case "*":{
+            stack[stack.length-2] = parseInt(stack[stack.length-1]) * parseInt(stack[stack.length-2]);
+            stack.pop();
+            break;
+        }
+        case "/":{
+            stack[stack.length-2] = parseInt(stack[stack.length-1]) / parseInt(stack[stack.length-2]);
+            stack.pop();
+            break;
+        }
+        case "%":{
+            stack[stack.length-2] = parseInt(stack[stack.length-1]) % parseInt(stack[stack.length-2]);
+            stack.pop();
+            break;
+        }
+        case "exile":{
+            stack.pop();
+            break;
+        }
+        case "simulacrum":{
+            stack.push(stack[stack.length-1]);
+            break;
+        }
+        case "print":{
+            console.log(stack[stack.length-1]);
+            break;
+        }
+        case "printS":{
+            console.log(stack);
+            break;
+        }
+        default:{
+            console.log(funcs);
+            // before you start, i didn't want switchception
+            if(x.startsWith('"')){
+                str.push(x.slice(1));
+                strStat = true;
+            }else if(strStat && x.endsWith('"')){
+                strStat = false;
+                str.push(x.slice(0,-1));
+                stack.push(str.join(" "));
+                str = [];
+            }else if(strStat){
+                // console.log(x);
+                str.push(x);
+            }else{
+                try{
+                    funcs.get(x);
+                    for(j in funcs.get(x)){
+                        exec(j);
+                    };
+                }catch(error){
+                    stack.push(x);
+                }
+            }
+            break;
+        }
+    }
+}
 
 async function main(){
     const pathto = process.argv[2];
@@ -36,94 +125,19 @@ async function main(){
     const program = p1.split(new splitspace);
     // console.log(program);
 
-    let stack = [];
-    let str = [];
-    let funcs = new Map();
-    let inProgFunc = [];
-    let funcIndex;
-    let strStat, funcStat;
-    // let i = 0;
-
     for(let x of program){
-        if(funcStat && x!="cut"){
-            inProgFunc.push(x);
-        }else if(funcStat && x=="cut"){
-            funcs.set(inProgFunc[0], )
-        }else{
-            switch(x){
-                case "": break;
-                case "scribe":{
-                    funcStat = true;
-                    funcIndex = i;
-                    break;
-                }
-                case "cut":{
-                    funcStat = false;
-                }
-                case "+":{
-                    stack[stack.length-2] = parseInt(stack[stack.length-1]) + parseInt(stack[stack.length-2]);
-                    stack.pop();
-                    break;
-                }
-                case "-":{
-                    stack[stack.length-2] = parseInt(stack[stack.length-1]) - parseInt(stack[stack.length-2]);
-                    stack.pop();
-                    break;
-                }
-                case "*":{
-                    stack[stack.length-2] = parseInt(stack[stack.length-1]) * parseInt(stack[stack.length-2]);
-                    stack.pop();
-                    break;
-                }
-                case "/":{
-                    stack[stack.length-2] = parseInt(stack[stack.length-1]) / parseInt(stack[stack.length-2]);
-                    stack.pop();
-                    break;
-                }
-                case "%":{
-                    stack[stack.length-2] = parseInt(stack[stack.length-1]) % parseInt(stack[stack.length-2]);
-                    stack.pop();
-                    break;
-                }
-                case "exile":{
-                    stack.pop();
-                    break;
-                }
-                case "simulacrum":{
-                    stack.push(stack[stack.length-1]);
-                    break;
-                }
-                case "print":{
-                    console.log(stack[stack.length-1]);
-                    break;
-                }
-                case "printS":{
-                    console.log(stack);
-                    break;
-                }
-                default:{
-                    // before you start, i didn't want switchception
-                    if(x.startsWith('"')){
-                        str.push(x.slice(1));
-                        strStat = true;
-                    }else if(strStat && x.endsWith('"')){
-                        strStat = false;
-                        str.push(x.slice(0,-1));
-                        stack.push(str.join(" "));
-                        str = [];
-                    }else if(strStat){
-                        console.log(x);
-                        str.push(x);
-                    }else{
-                        stack.push(x);
-                    }
-                    break;
-                }
-            }
-        }
-        // i++;
+        // if(funcStat && x!="cut"){
+        //     inProgFunc.push(x);
+        //     console.log(inProgFunc)
+        // }else if(funcStat && x=="cut"){
+        //     let funcName = inProgFunc[0];
+        //     inProgFunc.shift();
+        //     funcs.set(funcName, inProgFunc);
+        // }else{
+            exec(x);
+        // }
+        
     }
-    // console.log(stack);
 }
 
 main();
