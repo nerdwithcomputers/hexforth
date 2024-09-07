127b44dae8828f498609cdf493af85c0f914d3e4
nerdwithcomputers <hpscigeek@gmail.com>
2024-09-07 18:53:38 -0400

    YEAHAHAH FUNCTIONS F*CK YEHA IT WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOORRRRRRRRRRRRRRRRRRRRKKKKKKKKKKKKKKKKKKKKKKKKKSSSSSSSSSSSSSSSSSS

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 import fs from "fs";
 
 var stack = [];
-var str = [];
-var strStat;
 
 let funcs = new Map();
 var inProgFunc;
 var funcStat = false;
 // var funcIndex;
 
-class splitspace{
-    [Symbol.split](string){
-        let s1 = string.split(" ");
-        let s2 = [];
-        for(let x of s1){            
-            let y;
-            y = x.split("\n");
-            s2 = s2.concat(y);
-        }
-        return s2;
-    }
-};
 
 const readfile = (location) => 
 new Promise((resolve, reject) => {
@@ -80,14 +66,7 @@ function exec(x){
             break;
         }
         default:{
-            // console.log(funcs);
-            // console.log(stack);
-            // before you start, i didn't want switchception
-            if(x.startsWith('"')){
-                // start a string
-                str.push(x.slice(1));
-                strStat = true;
-            }else if(x.startsWith('scribe')){
+            if(x.startsWith('scribe(')){
                 // start scribing a function (or var, pretty much the same here)
                 funcStat = true;
                 funcs.set(x.substring(7,x.length-2), []);
@@ -95,9 +74,16 @@ function exec(x){
             }else if(x.startsWith('(')){
                 // call a function
                 let func = funcs.get(x.substring(1, x.length-1));
+                // console.log(func);
                 for(let i of func){
-                    mainLoop(i);
+                    exec(i);
                 }
+            }else if(x.startsWith('"') && x.endsWith('"')){
+                stack.push(x.substring(1,x.length-1));
+            }else if(x.startsWith(':')){
+                let compline = x.split(' ');
+                compline.shift();
+                for(let i of compline) exec(i);
             }else{
                 stack.push(x);
             }
@@ -109,21 +95,12 @@ function exec(x){
 function mainLoop(main){
 
     for(let x of main){
-        if(!strStat && !funcStat){
-            exec(x);
-            console.log(funcs);
-        } else if (strStat){
-            if(!x.endsWith('"')){
-                str.push(x.trim());
-            }else{
-                str.push(x.substring(0,x.length-1));
-                strStat = false;
-                stack.push(str.join(' '));
-                str = [];
-            }
-        } else if (funcStat){
-            if(x!='}'){
-                funcs.get(inProgFunc).push(x);
+        let y=x.trim();
+        if(!funcStat){
+            exec(y);
+        } else if(funcStat){
+            if(y!='}'){
+                funcs.get(inProgFunc).push(y);
             }else{
                 funcStat = false;
             }
@@ -136,8 +113,7 @@ async function main(){
     const pathto = process.argv[2];
     // console.log(pathto);
     const p1 = await readfile(pathto);
-    const program = p1.split(new splitspace);
-    // console.log(program);
+    const program = p1.split('\n');
 
     mainLoop(program);
 }
